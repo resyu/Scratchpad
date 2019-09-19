c906d1cfce4ebb77c40dc9ffcdadb2c6a13c629b
resyu <252769408@qq.com>
2019-09-19 17:16:28 +0800

    增加行列提示，及输出结果与源码的对应

--- views/index/index.js
+++ views/index/index.js
@@ -7,7 +7,9 @@ window.onload = function () {
     oResult = document.querySelector("#result"),
     oRun = document.querySelector("#run_btn"),
     oClear = document.querySelector("#clear_btn"),
-    oFocus = document.querySelector("#focus_btn"),
+    oSave = document.querySelector("#save_btn"),
+    aGetConsoleForLine = [],
+    aGetConsoleForLineBack = [],
     editor
 
   initFun()
@@ -19,27 +21,42 @@ window.onload = function () {
   oClear.onclick = function () {
     clearFun()
   }
-  //获得焦点
-  oFocus.onclick = function () {
-    editor.focus()
+  //保存
+  oSave.onclick = function () {
+    saveFun()
+  }
+  //行列
+  editor.on('cursorActivity', function () {
+    let pos = editor.getCursor()
+    document.querySelector('#state').innerHTML = `行 ${pos.line+1}，列 ${pos.ch}`
+  })
+  //resize
+  window.addEventListener('resize', function () {
+    initContentDivFun()
+  })
+  //init content div
+  function initContentDivFun() {
+    document.querySelector('#content').style.height = parseInt(window.innerHeight) - 30 + 'px'
+    document.querySelector('#result').style.height = parseInt(window.innerHeight) - 30 + 'px'
   }
   //初始化草稿纸
   function initFun() {
+    initContentDivFun()
     //CodeMirror configer
     editor = CodeMirror.fromTextArea(oEdit, {
-      mode: {
+      mode: { //模式
         name: 'javascript',
         json: true
       },
-      keyMap: "sublime",
-      lineNumbers: true,
-      lineWrapping: true,
-      styleActiveLine: true,
-      theme: "eclipse",
-      foldGutter: true,
-      matchBrackets: true,
-      gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter", "CodeMirror-lint-markers"],
-      showCursorWhenSelecting: true
+      keyMap: "sublime", //快捷键模式
+      lineNumbers: true, //行号
+      lineWrapping: true, //长文本换行
+      styleActiveLine: true, //当前行高亮
+      theme: "eclipse", //主题
+      foldGutter: true, //代码折叠
+      matchBrackets: true, //括号匹配
+      gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter", "CodeMirror-lint-markers"], //代码折叠相关
+      showCursorWhenSelecting: true //选中时是否显示光标
     })
     editor.focus()
     if (localStorage.inputValue) editor.setValue(localStorage.inputValue)
@@ -49,30 +66,59 @@ window.onload = function () {
     let inputStr = editor.getValue()
     if (inputStr) {
       let itemInput = '', //分段
-        aTempStr = [], //中间件
+        aTempStr = [], //按"console.log()"拆分
+        sContentTemp = '', //分段字符串中间件
         aItemOutput = [], //分段输出
         sResult = '', //最终输出
+        sCodeRun = '', //代码运行结果
         logKey = inputStr.match(regMatch) //匹配"console.log()"
 
-      if (logKey.length > 1) { //多个输出
+      if (logKey != null && logKey.length > 1) { //多个输出
         aTempStr = inputStr.trim().split(regMatch) //按"console.log()"拆分
-        for (let i = 0; i < aTempStr.length; i++) {
+        aGetConsoleForLine = getLineFun(logKey)
+        for (let i = 0; i < logKey.length; i++) {
           //得到要运行的分段字符串
-          itemInput = itemInput.concat(aTempStr[i].toString().concat(logKey[i]).replace(regReplace, "return"))
+          sContentTemp += aTempStr[i]
+          itemInput = sContentTemp.concat(logKey[i]).replace(regReplace, "return")
           //运行代码并把输出赋给aItemOutput
-          runFun(itemInput) && aItemOutput.push(runFun(itemInput))
-          itemInput = aTempStr[i].toString()
-        }
-        for (let i = 0; i < aItemOutput.length; i++) {
-          sResult += `${aItemOutput[i]}<br />`
+          sCodeRun = runFun(itemInput)
+          sCodeRun && aItemOutput.push(sCodeRun)
         }
       } else { //单个输出
         let codeStr = inputStr.replace(regReplace, "return") //要执行的code
-        sResult = runFun(codeStr)
+        aGetConsoleForLine = getLineFun(logKey)
+        sCodeRun = runFun(codeStr)
+        sCodeRun && aItemOutput.push(sCodeRun)
       }
       localStorage.inputValue = inputStr
+      for (let i = 0; i < aItemOutput.length; i++) {
+        sResult += `<p class="flex-row"><span>${aItemOutput[i]}</span><span class="line_tip">行 ${aGetConsoleForLine[i]+1}</span></p>`
+      }
       sResult && (oResult.innerHTML = sResult)
+      aGetConsoleForLineBack = [].concat(aGetConsoleForLine)
+      aGetConsoleForLine.length = 0
+      //点击行号跳转到对应的代码
+      for (let i = 0; i < document.querySelectorAll('.line_tip').length; i++) {
+        document.querySelectorAll('.line_tip')[i].addEventListener('click', function () {
+          editor.setCursor(aGetConsoleForLineBack[i])
+        })
+      }
     }
+  }
+  //根据内容获取行号
+  function getLineFun(sKey) {
+    let aContent = editor.getLineHandle(editor.lineCount() - 1).parent.lines,
+      nContent = 0
+    for (let i = 0; i < sKey.length; i++) {
+      for (let j = nContent; j < aContent.length; j++) {
+        if (sKey[i] === aContent[j].text) {
+          aGetConsoleForLine.push(j)
+          nContent = j + 1
+          break
+        }
+      }
+    }
+    return aGetConsoleForLine
   }
   //清屏
   function clearFun() {
@@ -87,21 +133,26 @@ window.onload = function () {
     try {
       return Function(code)()
     } catch (error) {
-      alert(error)
+      return error
     }
   }
   //快捷键
   document.onkeydown = function (event) {
     let e = event || window.event,
-      kc = e.keyCode || e.charCode;
+      kc = e.keyCode || e.charCode
+
     if (kc === 82 && e.ctrlKey) {
       consoleFun()
-    }
-    if (kc === 73 && e.ctrlKey) {
+    } else if (kc === 73 && e.ctrlKey) {
       editor.focus()
-    }
-    if (kc === 76 && e.ctrlKey) {
+    } else if (kc === 76 && e.ctrlKey) {
       clearFun()
+    } else if (kc === 83 && e.ctrlKey) {
+      saveFun()
     }
+  }
+  //保存
+  function saveFun() {
+    console.log('save')
   }
 }
